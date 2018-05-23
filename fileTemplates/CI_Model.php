2a8f3c7d83f05c47a788eccd592af3f89f837401
fabiogulotta <fbgulotta@gmail.com>
2018-05-23 02:22:15 +0200

    PS-2018.1.4 <Fabio@DESKTOP-TCB7456 Create Bonfire.xml, Smarty.tpl, api_temp.php, Default copy.xml, snappy-contrast.icls, CodeIgniter.xml, SQL Script.sql, rainbow.icls, snappy.icls, PHP File Header.php, darkside-contrast.icls, AngularJS .xml, laravel.icls, CI_Controller.php, PHP.xml, JavaScript.xml, Smarty.tpl, _@user_Material Darker.icls, CI_Model_header.php, NativeScript.xml, CodeIgniter_validation_rules.xml, User.xml, revelation.icls, tribal.icls, webServers.xml, piggy.icls, git.xml, _@user_Default.icls, zacks.icls, darkside.icls, juicy.icls, Twig.xml, solarflare.icls, JavaScript.xml, ui.lnf.xml, autocomplete.php, crud_model.php, projectView.xml, snappy-light.icls, PHP Bonfire Model.php, heroku.icls, html_xml.xml, Vue.xml, tron-contrast.icls, SQL.xml, stark.icls, arstotzka.icls, iceberg.icls, autocomplete.php, web-browsers.xml, userscape.icls, gloom-contrast.icls, Railscasts copy.icls, tron.icls, CI_Controller_header.php, project.default.xml, diff.xml, Smarty.xml, file.template.settings.xml, Blackboard copy2.icls, code.style.schemes.xml, carbonight-contrast.icls, halflife.icls, _@user_Darcula.icls, bold.icls, earthsong.icls, CodeIgniter.xml, slime.icls, PHP Bonfire Controller.php, CI_Controller.php, Default _2_.xml, gloom.icls, CI_Model.php, mellow-contrast.icls, SQL Script.sql, uml.xml, grunge.icls, frontier-contrast.icls, PHP Bonfire Model.php, hyrule-contrast.icls, stark-contrast.icls, Darkside copy.icls, php.xml, JavaScript Testing.xml, watcherDefaultTasks.xml, colors.scheme.xml, revelation-contrast.icls, Default copy 1.xml, lavender.icls, chocolate.icls, html_xml.xml, azure.icls, earthsong-contrast.icls, boxuk.icls, mellow.icls, kiwi.icls, turnip.icls, laravel-contrast.icls, Zen HTML.xml, hg.xml, laf.xml, Smarty.tpl, Monokai copy.icls, goldfish-contrast.icls, hyrule.icls, CI_Model_header.php, material_theme.xml, Smarty.tpl, vcs.xml, peacock-contrast.icls, usageView.xml, freshcut-contrast.icls, markdown.xml, patriot.icls, sourlick.icls, peacock.icls, angularTemplates.xml, Default.xml, shrek.icls, find.xml, Darcula copy2.icls, keymap.xml, peacocks-in-space.icls, super.icls, Blackboard copy.icls, Angular2 TypeScript Live Templates.xml, CodeIgniter_validation_rules.xml, CI_Model.php, Default.xml, freshcut.icls, slime-contrast.icls, goldfish.icls, slate.icls, keen.icls, potpourri.icls, Example.php, grunge-contrast.icls, editor.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, glowfish-contrast.icls, WarmNeon copy.icls, yule.icls, code.style.schemes, patriot-contrast.icls, notifications.xml, tonic.icls, halflife-contrast.icls, api_temp.php, frontier.icls, carbonight.icls, turnip-contrast.icls, Twilight copy2.icls, Default _1_.xml, peel.icls, legacy.icls, IntelliLang.xml, mud-contrast.icls, keen-contrast.icls, AngularJS .xml, applicationLibraries.xml, ignore.xml, spearmint.icls, Example.php, earthsong-light.icls, Zen HTML.xml, Darcula copy.icls, PHP.xml, Css.xml, PHP Bonfire Controller.php, zacks-contrast.icls, potpourri-contrast.icls, glowfish.icls, editor.codeinsight.xml, otakon.icls, crisp.icls, iceberg-contrast.icls, juicy-contrast.icls, other.xml, Twilight copy.icls, _@user_Material Monokai Pro.icls, solarflare-contrast.icls, lavender-contrast.icls, _@user_Material Lighter.icls, templates.xml, emmet.xml, github.icls, Angular.xml, crud_model.php, pastel.icls, Smarty.xml, path.macros.xml, sourlick-contrast.icls, PHP File Header.php, intentionSettings.xml, mintchoc.icls, keyboard.xml, _@user_Atom One Dark.icls, HTTP Request.xml, databaseSettings.xml, mud.icls, CI_Controller_header.php

---
+++ fileTemplates/CI_Model.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  Comande_model.php Controller
+ *
+ *
+ *  This work is licensed under the Creative Commons Attribution 3.0 Unported
+ *  License. To view a copy of this license, visit
+ *  http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative
+ *  Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ *
+ * @filename   Comande_model.php
+ * @category   controllers
+ * @subpackage articles
+ * @package    Bonfire
+ * @author     Shawn Crigger <support@s-vizion.com>
+ * @copyright  2011-{2016} S-Vizion Software and Developments
+ * @license    http://creativecommons.org/licenses/by/3.0/  CCA3L
+ * @version    1.5.0 (last revision: 09 10, 2016)
+ * @date       10/09/2016
+ *
+ *  //--------------------------------------------------------------------
+ *
+ *  I define all my dependencies in my class doc block's so that my auto-completion works better and more importantly
+ *  when I have to work on this again after 2 years, I know what is needed and used, that way I can update stuff easier.
+ *
+ *
+ * @property CI_Loader $load
+ * @property CI_Lang $lang
+ * @property CI_Form_validation $form_validation
+ *
+ */
+class $MODEL$_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
