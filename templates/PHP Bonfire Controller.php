3b3e146beda27d5ad46736739eed8734ffb711f2
FabioGi <fbgulotta@gmail.com>
2018-09-28 11:16:35 +0200

    PS-2018.2.4 <Administrator@dc01 Delete Bonfire.xml, Smarty.tpl, api_temp.php, Default copy.xml, snappy-contrast.icls, CodeIgniter.xml, SQL Script.sql, rainbow.icls, _@user_Material Oceanic.icls, snappy.icls, PHP File Header.php, darkside-contrast.icls, AngularJS .xml, React.xml, laravel.icls, CI_Controller.php, PHP.xml, JavaScript.xml, Smarty.tpl, _@user_Material Darker.icls, CI_Model_header.php, NativeScript.xml, CodeIgniter_validation_rules.xml, User.xml, revelation.icls, tribal.icls, webServers.xml, piggy.icls, git.xml, _@user_Default.icls, CodeGlance.xml, zacks.icls, darkside.icls, juicy.icls, Twig.xml, solarflare.icls, JavaScript.xml, ui.lnf.xml, autocomplete.php, crud_model.php, filetypes.xml, projectView.xml, snappy-light.icls, PHP Bonfire Model.php, plugin_ui.xml, heroku.icls, html_xml.xml, Default for Laptop.xml, Vue.xml, tron-contrast.icls, SQL.xml, stark.icls, arstotzka.icls, iceberg.icls, autocomplete.php, web-browsers.xml, userscape.icls, gloom-contrast.icls, Railscasts copy.icls, tron.icls, CI_Controller_header.php, project.default.xml, diff.xml, Smarty.xml, file.template.settings.xml, Blackboard copy2.icls, carbonight-contrast.icls, halflife.icls, _@user_Darcula.icls, bold.icls, earthsong.icls, CodeIgniter.xml, slime.icls, PHP Bonfire Controller.php, CI_Controller.php, Default _2_.xml, gloom.icls, CI_Model.php, mellow-contrast.icls, SQL Script.sql, _@user_Darcula-dc01.icls, uml.xml, grunge.icls, frontier-contrast.icls, PHP Bonfire Model.php, hyrule-contrast.icls, stark-contrast.icls, Darkside copy.icls, log_highlighting.xml, php.xml, JavaScript Testing.xml, watcherDefaultTasks.xml, colors.scheme.xml, revelation-contrast.icls, Default copy 1.xml, lavender.icls, chocolate.icls, html_xml.xml, azure.icls, earthsong-contrast.icls, boxuk.icls, mellow.icls, kiwi.icls, turnip.icls, laravel-contrast.icls, Zen HTML.xml, hg.xml, laf.xml, Smarty.tpl, Covalent.xml, Monokai copy.icls, goldfish-contrast.icls, hyrule.icls, CI_Model_header.php, material_theme.xml, Smarty.tpl, dbnavigator.xml, vcs.xml, peacock-contrast.icls, usageView.xml, freshcut-contrast.icls, markdown.xml, patriot.icls, sourlick.icls, peacock.icls, angularTemplates.xml, Default.xml, shrek.icls, find.xml, dataSources.xml, Darcula copy2.icls, keymap.xml, peacocks-in-space.icls, super.icls, Blackboard copy.icls, Angular2 TypeScript Live Templates.xml, CodeIgniter_validation_rules.xml, CI_Model.php, Default.xml, freshcut.icls, slime-contrast.icls, goldfish.icls, slate.icls, keen.icls, potpourri.icls, Example.php, grunge-contrast.icls, editor.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, glowfish-contrast.icls, Angular Templates.xml, WarmNeon copy.icls, yule.icls, code.style.schemes, patriot-contrast.icls, highlight-bracket-pair-plugin.xml, notifications.xml, tonic.icls, databaseDrivers.xml, halflife-contrast.icls, api_temp.php, frontier.icls, Jquery.xml, carbonight.icls, material_custom_theme.xml, turnip-contrast.icls, Twilight copy2.icls, Default _1_.xml, peel.icls, legacy.icls, IntelliLang.xml, mud-contrast.icls, keen-contrast.icls, AngularJS .xml, applicationLibraries.xml, ignore.xml, spearmint.icls, Example.php, earthsong-light.icls, Zen HTML.xml, Darcula copy.icls, PHP.xml, Css.xml, PHP Bonfire Controller.php, zacks-contrast.icls, potpourri-contrast.icls, glowfish.icls, CSS-X-Fire.xml, editor.codeinsight.xml, otakon.icls, crisp.icls, iceberg-contrast.icls, juicy-contrast.icls, Twilight copy.icls, _@user_Material Monokai Pro.icls, solarflare-contrast.icls, lavender-contrast.icls, _@user_Material Lighter.icls, Flex layout.xml, templates.xml, emmet.xml, github.icls, Angular.xml, crud_model.php, pastel.icls, Smarty.xml, path.macros.xml, sourlick-contrast.icls, PHP File Header.php, intentionSettings.xml, mintchoc.icls, keyboard.xml, _@user_Atom One Dark.icls, HTTP Request.xml, databaseSettings.xml, mud.icls, CI_Controller_header.php

--- templates/PHP Bonfire Controller.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-#parse("CI_Controller_header") 
-class ${NAME} extends Front_Controller {
-
-}
-
-/* end of ${FILE_NAME} */
-/* end of ./${DIRECTORY}/${FILE_NAME} */
